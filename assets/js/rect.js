25217447ebc2e8d712d0eb7cc4d4706d7cd5e437
jordan <jordancooperman@gmail.com>
2011-10-19 22:19:31 -0400

    organized assets

---
+++ assets/js/rect.js
@@ -0,0 +1,45 @@
+function Rect() {
+  var rect = document.createElement('div');
+  this.class;
+
+  this.posX;
+  this.posY;
+
+  this.velX = 0;
+
+  this.width = 10;
+  this.height = 10;
+
+  this.target;
+
+  this.init = function() {
+    rect.className = this.class;
+    document.body.appendChild(rect);
+  }
+
+  this.update = function() {
+    var diff = this.target - this.posX;
+
+    this.velX *= 0;
+    diff *= 0.8;
+
+    this.velX += diff;
+    this.posX += this.velX;
+  }
+
+  this.render = function(ctx) {
+    ctx.fillStyle = "#5DFC0A";
+    ctx.fillRect(this.posX, this.posY, this.width, this.height);
+    ctx.fill();
+  }
+
+  this.renderHtml = function() {
+    rect.style.width = this.width + "px";
+    rect.style.height = this.height + "px";
+    rect.style.position = 'absolute';
+    rect.style.left = this.posX + "px";
+    rect.style.top = this.posY + "px";
+    rect.style.backgroundColor = "#fff";
+  }
+}
+
